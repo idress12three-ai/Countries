5062fdcc971842f07e5a4c414e407019953cdb8d
idress12three-ai <idress12three@gmail.com>
2026-01-20 16:55:07 +0000

    done

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,14 +1,42 @@
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
-
+      private String name;
+      private String capital;
+      private String language;
+      private String imagefile;
   // add constructors
-
+      public Country (String name,String capital,String language,String imagefile){
+              this.name=name;
+              this.capital=capital;
+              this.language=language;
+              this.imagefile=imagefile;
+      }
+      public Country(){
+            name="default";
+            capital="default";
+            language="default";
+            imagefile="default";
+      }
   // Write accessor/get methods for each instance variable that returns it.
-
+      public String getname(){
+        return name;
+      }
+      public String getcapital(){
+        return capital;
+      }
+      public String getlanguage(){
+        return language;
+      }
+      public String getimagefile(){
+        return imagefile;
+      }
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
- 
+      public String toString(){
+            return (name + "'s capital is " + capital + " and its primary language is " + language + ".");
+      }
 
 
   
 }
+
--- workspace/Main.java
+++ workspace/Main.java
@@ -17,7 +17,8 @@ public class Main
   private ImageIcon img;
   private JLabel imageLabel;
   private JLabel outputLabel;
-  
+  private JTextArea userInput;
+
   public static void main(String[] args) {
     // Create the GUI
     Main gui = new Main();
@@ -26,27 +27,44 @@ public class Main
   }
 
   /* loadCountries() reads in the data from the countries-data.csv file and fills in the countryArray with data. You need to add the loop that reads in the country data into the array. */
-  public void loadCountries() 
-  {
+  public void loadCountries(){
+    try{
     // Open the data file. Please note that the file structure we're working with requires the full file path as shown here unlike what you saw in runestone where the file name was sufficient.
     File file = new File("/workspaces/Countries/workspace/countries-data.csv");
+    Scanner scan= new Scanner(file);
+    int i=0;
     
     //create a scanner and a loop to read from the file until you've read everything.
     // inside the loop you'll need to read in a line from the file and use "split" to break up the data into destinct parts.
     // create a new Country using your constructor with 4 arguments (each of the arguments is a different part of the line you've read in)
     // inside the loop, set countryArray[i] to the created Country object
     //after running this method your array should contain all 10 countries from inside the countries-data file.
-     
+    while (scan.hasNext() && i<countryArray.length){
+            String line = scan.nextLine();
+            String []  split = line.split(",");
+            
+            countryArray[i]= new Country(split[0],split[1],split[2],split[3]);
+            i++;
+            
+            
+    }
+    scan.close();
+    }catch(IOException e){
+      System.out.println("file couldnt be opened");
+    } 
     
-  }
+ 
+}
+  
+    
 
   /* showCountry() will show the image associated with the current country. It should get the country at index from the countryArray. It should use its get method to get its image file name and use the code below to put the image in the GUI.
   */
   public void showCountry() {
     // Get the country at index from countryArray
-    
+    Country c = countryArray[index];
     // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
+    String imagefile = c.getimagefile(); 
     // Use the following code to create an new Image Icon and put it into the GUI
     img = new ImageIcon("/workspaces/Countries/workspace/"+imagefile);
     imageLabel.setIcon(img);
@@ -55,28 +73,43 @@ public class Main
   /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
   public void nextButtonClick()
   {
-    
+    index++;
+    if (index>=countryArray.length){
+      index=0;
+    }
+    outputLabel.setText("");
+    userInput.setText("");
+    showCountry();
+
   }
   
   /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
   public void reviewButtonClick()
   {
-     
+    Country c = countryArray[index];
+    String answer = userInput.getText().trim();
+    String info = c.toString();
+    System.out.println(info);
+    outputLabel.setText(info);
+    
   }
 
   /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
   */
   public void quizButtonClick()
   {
-    Scanner scan = new Scanner(System.in); 
-    
-    
-    
-  }
+    outputLabel.setText("");
+    Country c = countryArray[index];
+    String answer = userInput.getText().trim();
 
-
-
-
+    if(answer.equalsIgnoreCase((c.getcapital()))){
+         outputLabel.setText("Correct!");
+     } else {
+         outputLabel.setText("Incorrect! The capital of " + c.getname() + " is " + c.getcapital() + ".");
+     }
+     userInput.setText("");
+    }
+  
   /* You are not required to change anythign below here. You do so at your own risk! */
   /* The Main() constructor is finished and will construct the GUI */
 public Main() {
@@ -98,6 +131,9 @@ public Main() {
         // and one for output
         outputLabel = new JLabel();
         jFrame.add(imageLabel);
+        userInput= new JTextArea(1,40);
+        jFrame.add(userInput);
+        jFrame.add(userInput);
         jFrame.add(outputLabel);
         jFrame.setVisible(true);
         // add event listener for button click
